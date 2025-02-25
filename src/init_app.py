569ada74486292699f0d33397cacd77afb1157a2
Gleb Lazarev <glazarev@astralinux.ru>
2025-02-25 23:49:10 +0300

    Убрал еще импорт

--- src/init_app.py
+++ src/init_app.py
@@ -4,7 +4,7 @@ init
 from telegram.ext import Application
 from src.settings import settings
 # pylint: disable=unused-import
-from .controller import Controller, Invoice
+from .controller import Controller
 from .db_client.core import DbClient
 
 
